34f46f9e041072af723723bdf8649509b4ddad21
cr1127 <56252285+cr1127@users.noreply.github.com>
2019-11-30 21:16:25 +0800

    Update design_Patterns.cpp

--- design_Patterns.cpp
+++ design_Patterns.cpp
@@ -1,6 +1,27 @@
 1.设计原则（6个）
 
-	对于正整数而言，取模运算和取余运算时一样的。
+	1）单一职责原则( Single Responsibility Principle SRP)
+
+	RBAC模型( Role-Based Access Control)
+
+	定义是 应该有且仅有一个原因引起类的变更。
+
+	SRP的原话解释是：There should never be more than one reason for a class to change.
+
+	SRP提出一个编写程序的标准，用职责或变化原因来衡量接口或类设计得是否优良，但是职责和变化原因都是不可度量的，因项目而异，因环境而异。
+
+	接口一定要做到单一职责，类的设计尽量做到只有一个原因引起变化。
+
+	2）里氏替换原则( Liskov Substitution Principle LSP)
+
+	定义是 Fucntion that use pointers or referencees to base classes must be able to use objects of derived classes without knowing  it.
+
+	规范（4个）：
+
+	
+
+
+
 
 
 
